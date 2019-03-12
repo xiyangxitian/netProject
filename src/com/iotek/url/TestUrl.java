132892f513925bf62ba08ac103318c86c6a7e927
xiyangxitian <liyan@live.shop.edu.cn>
2019-03-12 09:30:55 +0800

    URL获取资源

---
+++ src/com/iotek/url/TestUrl.java
@@ -0,0 +1,8 @@
+package com.iotek.url;
+
+public class TestUrl {
+    public static void main(String[] args) {
+//        UrlUtil.downLoad("http://p0.so.qhimgs1.com/sdr/200_200_/t0195caf55de65163a0.jpg", "e:/io", "preetyGirl.jpg");
+        UrlUtil.downLoad("http://dl.image.so.com/d?imgurl=http%3A%2F%2Fp0.so.qhimgs1.com%2Ft0195caf55de65163a0.jpg&purl=http%3A%2F%2Fimage.so.com%2F%3Fsrc%3Ddl.image&key=5e737122eb", "e:/io", "pretyGirl3.jpg");
+    }
+}
---
+++ src/com/iotek/url/UrlUtil.java
@@ -0,0 +1,49 @@
+package com.iotek.url;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * 从指定url下载资源
+ */
+public class UrlUtil {
+
+    public static void downLoad(String urlPath, String savePath, String fileName) {
+        InputStream is = null;
+        FileOutputStream fos = null;
+        try {
+            URL url = new URL(urlPath);
+//            URLConnection urlConnection = url.openConnection();
+//            is = urlConnection.getInputStream();
+            is = url.openStream();
+            File file = new File(savePath);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            fos = new FileOutputStream(file.getAbsolutePath() + "/" + fileName);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = is.read(buf)) != -1) {
+                fos.write(buf, 0, len);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+}
